4792cb722c471a6e5cd0d5d58c8c4165eec46df5
AndreyGra <andrey@chabunin.com>
2020-08-04 18:13:43 +1000

    Added header w/ struct definition for MCP4922 handle, and enums for control bits

---
+++ Inc/MCP4922.h
@@ -0,0 +1,32 @@
+#ifndef MCP4922_H
+#define MCP4922_H
+
+#include <stdint.h>
+
+enum MCP4922_CHANNEL_SELECTION {
+    A = 0, B = 1
+};
+
+enum MCP4922_OUTPUT_GAIN {
+    X2 = 0, X1 = 1
+};
+
+
+enum MCP4922_INPUT_STATE{
+    BUFFERED = 1, UNBUFFERED = 0
+} ;
+
+
+enum MCP4922_CHANNEL_STATUS {
+    ACTIVE = 1, SHUTDOWN = 0
+};
+
+typedef struct {
+   MCP4922_CHANNEL_SELECTION channel;
+   MCP4922_INPUT_STATE inputState;
+   MCP4922_OUTPUT_GAIN gain;
+   MCP4922_CHANNEL_STATUS shutdownStatus;
+   uint16_t channelValue;
+} MCP4922_Handle_Typedef ; 
+
+#endif
